116e237f59689f1e6763a60b3e637bd8e3c57080
Lix3nn53 <lixakyildiz@gmail.com>
2020-01-18 23:01:25 +0300

    remove rp message on login

--- src/main/java/io/github/lix3nn53/guardiansofadelia/lobby/events/MyLoginEvent.java
+++ src/main/java/io/github/lix3nn53/guardiansofadelia/lobby/events/MyLoginEvent.java
@@ -18,22 +18,5 @@ public class MyLoginEvent implements Listener {
         p.sendTitle(ChatColor.GREEN + "Welcome!", ChatColor.YELLOW + "[ " + ChatColor.GOLD +"Guardians " +
                 ChatColor.YELLOW +"of " + ChatColor.GREEN + "Adelia " + ChatColor.YELLOW + "]", 20, 80, 20);
         p.setFoodLevel(20);
-
-        p.sendMessage(ChatColor.YELLOW + "If server resource pack is not loaded: ");
-        TextComponent messageMain = new TextComponent("Click to load resource pack!");
-        messageMain.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rp load"));
-        messageMain.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Load server RP").color(ChatColor.GREEN).create()));
-        messageMain.setBold(true);
-        messageMain.setColor(ChatColor.GREEN);
-
-        TextComponent messageReject = new TextComponent("Help!");
-        messageReject.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://guardiansofadelia.herokuapp.com/"));
-        messageReject.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Go to website").color(ChatColor.YELLOW).create()));
-        messageReject.setBold(true);
-        messageReject.setColor(ChatColor.YELLOW);
-
-        messageMain.addExtra("    ");
-        messageMain.addExtra(messageReject);
-        p.spigot().sendMessage(messageMain);
     }
 }
